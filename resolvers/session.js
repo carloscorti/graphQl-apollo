295197c2e318f81bf8daae553124097019317936
Carlos <carlosacorti@hotmail.com>
2020-10-17 19:21:48 +0200

    handling resolver errors
    -on resolvers/session.js: catch errors on
    speakerAPI.getSpeakerById(speaker.id) speakers REST API call

--- resolvers/session.js
+++ resolvers/session.js
@@ -1,3 +1,5 @@
+const { ApolloError } = require('apollo-server');
+
 const Session = {
   // speakers: async (parent, args, { dataSources }, info) => {
   // in this case parent is the session where speaker is
@@ -9,17 +11,36 @@ const Session = {
     //   );
     // }
     // return filteredSpeakers;
-    return session.speaker
-      ? session.speakers.map(
-          async (speaker) =>
-            await dataSources.speakerAPI.getSpeakerById(speaker.id)
-        )
-      : [];
+    // return session.speaker
+    //   ? session.speakers.map(async (speaker) => {
+    //       try {
+    //         return await dataSources.speakerAPI.getSpeakerById(speaker.id);
+    //       } catch (err) {
+    //         return null;
+    //       }
+    //     })
+    //   : [];
+    return session.speakers.map(async (speaker) => {
+      try {
+        return await dataSources.speakerAPI.getSpeakerById(speaker.id);
+      } catch (err) {
+        return new ApolloError('Unable to retrive speakers', 'SPIKERAPIERROR', {
+          token: 'uniqueTokenToSaveOnDataBaseToTraceErrors',
+        });
+      }
+    });
   },
   // speakers: async (session, args, { dataSources }, info) => {
-  //   const speakers = await dataSources.speakerAPI.getSpeakers();
-  //   const speakersId = session.speakers.map((speaker) => speaker.id);
-  //   return speakers.filter((speaker) => speakersId.includes(speaker.id));
+  //   try {
+  //     const speakers = await dataSources.speakerAPI.getSpeakers();
+  //     const speakersId = session.speakers.map((speaker) => speaker.id);
+  //     return speakers.filter((speaker) => speakersId.includes(speaker.id));
+  //   } catch (err) {
+  //     console.log('catched errro');
+  //     return new ApolloError('Unable to retrive speakers', 'SPIKERAPIERROR', {
+  //       token: 'uniqueTokenToSaveOnDataBaseToTraceErrors',
+  //     });
+  //   }
   // },
 };
 
